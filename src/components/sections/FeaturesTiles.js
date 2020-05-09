6fdc564258c722c9d49abd7f1a8332284d5bbdeb
DanielSselugo <danielsselugo@gmail.com>
2020-05-10 01:20:17 +0300

    updated

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,214 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionTilesProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionTilesProps.types,
+};
+
+const defaultProps = {
+  ...SectionTilesProps.defaults,
+};
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-tiles section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-tiles-inner section-inner pt-0",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const tilesClasses = classNames(
+    "tiles-wrap center-content",
+    pushLeft && "push-left"
+  );
+
+  const sectionHeader = {
+    title: "Always follow the Prevention & Control Guidelines",
+    paragraph:
+      "It is important to StayHome to StaySafe in order to safe-guard ourselves from this Viral pandemic.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-01.svg")}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Wash your Hands</h4>
+                  <p className="m-0 text-sm">
+                    Use any soapy detergent or alcoholic sanitizer to wash your
+                    hands.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-02.svg")}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Wear A Mask/Gear</h4>
+                  <p className="m-0 text-sm">
+                    When you attempt to move away from your premises, wear a
+                    mask or a protective gear and also when you are going to
+                    public places.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="400"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-03.svg")}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Keep 5-METER Distance</h4>
+                  <p className="m-0 text-sm">
+                    When you're discussing with other people make sure that
+                    you're 4-5 meter apart. This will create a nice environment
+                    both parties not to spread the Virus.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-04.svg")}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Avoid contact</h4>
+                  <p className="m-0 text-sm">
+                    Always avoid contact with your face, nose and mouth before
+                    washing your hands thoroughly with soap, also with other
+                    people in your community.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-05.svg")}
+                      alt="Features tile icon 05"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Always STAY-HOME</h4>
+                  <p className="m-0 text-sm">
+                    To prevent the rampant spread of COVID-19, always StayHome
+                    in case you don't have a crucial reason to move.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="400"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-06.svg")}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Call Help</h4>
+                  <p className="m-0 text-sm">
+                    In case you predict a person in your region having signs of
+                    Covid-19, try to reach out the Ministry of Health help
+                    lines. 0800302032/086756454
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
